f423840bb4bc27ce5eaf8c59a3c4105dce6bead6
Mszauer <mszauer@mail.usf.edu>
2015-08-27 22:20:57 -0700

    drew aku

--- MainWindow.cs
+++ MainWindow.cs
@@ -7,30 +7,30 @@ using GameFramework;
 namespace ConsoleApplication1 {
     class MainWindow {
         public static OpenTK.GameWindow Window = null; //reference to OpenTK window
-        static int robot = -1;
-        static int currentRotation = 0;
+        static int texBird = -1;
+        static int texJack = -1;
+        static int texAku = -1;
         public static void Initialize(object sender, EventArgs e) {
-            robot = TextureManager.Instance.LoadTexture("Assets/machine.png");
             GraphicsManager.Instance.Initialize(Window);
             TextureManager.Instance.Initialize(Window);
+
+            texBird = TextureManager.Instance.LoadTexture("Assets/Bird.png");
+            texJack = TextureManager.Instance.LoadTexture("Assets/Jack.png");
+            texAku = TextureManager.Instance.LoadTexture("Assets/Aku.png");
         }
         public static void Update(object sender, FrameEventArgs e) {
 
         }
         public static void Render(object sender, FrameEventArgs e) {
             GraphicsManager.Instance.ClearScreen(Color.CadetBlue);
-            TextureManager.Instance.Draw(robot, new Point(0, 0));
-            /*
-            //draw each piece individually
-            TextureManager.Instance.Draw(robot, new Point(128, 0), 1.0f, new Rectangle(128, 0, 128, 128));
-            TextureManager.Instance.Draw(robot, new Point(168, 31), .75f, new Rectangle(0, 64, 64, 64));
-
-             */
+            TextureManager.Instance.Draw(texAku, new Point(0, 0));
             GraphicsManager.Instance.SwapBuffers();
         }
         public static void Shutdown(object sender, EventArgs e) {
-            TextureManager.Instance.UnloadTexture(robot);
-            robot = -1;
+            TextureManager.Instance.UnloadTexture(texBird);
+            TextureManager.Instance.UnloadTexture(texJack);
+            TextureManager.Instance.UnloadTexture(texAku);
+            texBird = texAku = texJack = -1;
             TextureManager.Instance.Shutdown();
             GraphicsManager.Instance.Shutdown();
         }
