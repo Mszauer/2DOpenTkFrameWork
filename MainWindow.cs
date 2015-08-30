bb3b882c30e05ca7932e639ae6b6838ac12b053b
Mszauer <mszauer@mail.usf.edu>
2015-08-30 14:52:13 -0700

    finished joystick example minus on your own

--- MainWindow.cs
+++ MainWindow.cs
@@ -9,9 +9,10 @@ using System.Collections.Generic;
 namespace ConsoleApplication1 {
     class MainWindow {
         public static OpenTK.GameWindow Window = null; //reference to OpenTK window
-        
-        static List<OpenTK.Input.Key> input = new List<OpenTK.Input.Key>();
-        static RectangleF position = new RectangleF(100f, 100f, 20f, 20f);
+
+        private static RectangleF position = new RectangleF(100, 100, 20, 20);
+        private static Color[] colors = new Color[] { Color.Red, Color.Blue, Color.Green, Color.Yellow };
+        private static int currentColor = 0;
 
         public static void Initialize(object sender, EventArgs e) {
             GraphicsManager.Instance.Initialize(Window);
@@ -19,33 +20,80 @@ namespace ConsoleApplication1 {
 
         }
         public static void Update(object sender, FrameEventArgs e) {
+            InputManager.Instance.Update();
+            if (InputManager.Instance.APressed(0)) {
+                currentColor += 1;
+                if (currentColor >= colors.Length) {
+                    currentColor = 0;
+                }
+            }
+            if (InputManager.Instance.LeftStickX(0) < 0) { // < 0 = left
+                position.X -= 80.0f * Math.Abs(InputManager.Instance.LeftStickX(0)) * (float)e.Time;
+            }
+            else if (InputManager.Instance.LeftStickX(0) > 0) { // < 0 = right
+                position.X += 80.0f * Math.Abs(InputManager.Instance.LeftStickX(0)) * (float)e.Time;
+            }
 
-            input.Clear();
-            input.AddRange(InputManager.Instance.GetAllKeysDown());
-            if (InputManager.Instance.KeyDown(OpenTK.Input.Key.Up) || InputManager.Instance.KeyDown(OpenTK.Input.Key.W)) {
-                position.Y -= (float)(50f * e.Time);
+            if (InputManager.Instance.LeftStickY(0) < 0) { // y < 0 = down
+                position.Y += 80.0f * Math.Abs(InputManager.Instance.LeftStickY(0)) * (float)e.Time;
             }
-            if (InputManager.Instance.KeyDown(OpenTK.Input.Key.Down) || InputManager.Instance.KeyDown(OpenTK.Input.Key.S)) {
-                position.Y += (float)(50f * e.Time);
-            }
-            if (InputManager.Instance.KeyDown(OpenTK.Input.Key.Right) || InputManager.Instance.KeyDown(OpenTK.Input.Key.D)) {
-                position.X += (float)(50f * e.Time);
-            }
-            if (InputManager.Instance.KeyDown(OpenTK.Input.Key.Left) || InputManager.Instance.KeyDown(OpenTK.Input.Key.A)) {
-                position.X -= (float)(50f * e.Time);
+            else if (InputManager.Instance.LeftStickY(0) > 0) { // y > 0 = up
+                position.Y -= 80.0f * Math.Abs(InputManager.Instance.LeftStickY(0)) * (float)e.Time;
             }
 
             InputManager.Instance.Update();
         }
         public static void Render(object sender, FrameEventArgs e) {
             GraphicsManager.Instance.ClearScreen(Color.CadetBlue);
+            if (!InputManager.Instance.IsConnected(0)) {
+                GraphicsManager.Instance.DrawString("Please connect a controller", new PointF(10,10),Color.Red);
+            }
+            else if(!InputManager.Instance.HasAButton(0)){
+                GraphicsManager.Instance.DrawString("A button not mapped, press A button", new PointF(10, 10), Color.Red);
+                JoystickButton newAButton = JoystickButton.Button0;
+                if (InputManager.Instance.GetButton(0, ref newAButton)) {
+                    InputManager.Instance.GetMapping(0).A = newAButton;
+                }
+            }
+            else if (!InputManager.Instance.HasLeftStick(0)) {
+                InputManager.ControllerMapping map = InputManager.Instance.GetMapping(0);
 
-            GraphicsManager.Instance.DrawRect(position, Color.Red);
-            GraphicsManager.Instance.DrawString("Keys down: ", new Point(0, 0), Color.Black);
-            GraphicsManager.Instance.DrawString("Space key is up: " + InputManager.Instance.KeyDown(OpenTK.Input.Key.Space), new Point(20, 20), Color.Black);
-            GraphicsManager.Instance.DrawString("Space key is down: " + InputManager.Instance.KeyUp(OpenTK.Input.Key.Space), new Point(20, 40), Color.Black);
-            GraphicsManager.Instance.DrawString("Space key is pressed: " + InputManager.Instance.KeyPressed(OpenTK.Input.Key.Space), new Point(20, 60), Color.Black);
-            GraphicsManager.Instance.DrawString("Space key is released: " + InputManager.Instance.KeyReleased(OpenTK.Input.Key.Space), new Point(20, 80), Color.Black);
+                if (!map.HasLeftAxisX) {
+                    GraphicsManager.Instance.DrawString("X axis is not found, move left stick horizontally", new PointF(10, 10), Color.Red);
+                }
+                else if (!map.HasLeftAxisY) {
+                    GraphicsManager.Instance.DrawString("Y axis is not mouve, move left stick vertically", new PointF(10, 10), Color.Red);
+                }
+                JoystickAxis newAxis = JoystickAxis.Axis0;
+                if (map.HasLeftAxisX) {
+                    if (InputManager.Instance.GetAxis(0, ref newAxis, map.LeftAxisX)) {
+                        map.LeftAxisY = newAxis;
+                    }
+                }
+                else {
+                    if (InputManager.Instance.GetAxis(0, ref newAxis)) {
+                        map.LeftAxisX = newAxis;
+                    }
+                }
+            }
+            else {
+                GraphicsManager.Instance.DrawRect(position, colors[currentColor]);
+            }
+            int y = 0;
+            JoystickState state = Joystick.GetState(0);
+            InputManager.ControllerMapping buttons = InputManager.Instance.GetMapping(0);
+            foreach (JoystickAxis enumVal in Enum.GetValues(typeof(JoystickAxis))) {
+                Color clr = Color.Black;
+                if (enumVal == buttons.LeftAxisX) {
+                    clr = Color.Purple;
+                }
+                if (enumVal == buttons.LeftAxisY) {
+                    clr = Color.Blue;
+                }
+                GraphicsManager.Instance.DrawString(enumVal.ToString() + ": " + state.GetAxis(enumVal), new PointF(10,30+y),clr);
+                y += 20;
+            }
+            GraphicsManager.Instance.DrawRect(position, colors[currentColor]);
 
             GraphicsManager.Instance.SwapBuffers();
         }
